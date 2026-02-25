a7dac02e4716959e9f64434df45a878ee5788b36
Matthew Macdonald-Wallace <matt@doics.co>
2026-02-25 10:43:08 +0000

    Serve Django static assets in container runserver mode

--- docker/space_weather-entrypoint.sh
+++ docker/space_weather-entrypoint.sh
@@ -10,4 +10,4 @@ exec opentelemetry-instrument \
   --service_name "${OTEL_SERVICE_NAME:-space-weather}" \
   --exporter_otlp_endpoint "${OTEL_EXPORTER_OTLP_ENDPOINT:-http://otel-collector:4318}" \
   --exporter_otlp_protocol "${OTEL_EXPORTER_OTLP_PROTOCOL:-http/protobuf}" \
-  python manage.py runserver 0.0.0.0:8889 --noreload
+  python manage.py runserver 0.0.0.0:8889 --noreload --insecure
