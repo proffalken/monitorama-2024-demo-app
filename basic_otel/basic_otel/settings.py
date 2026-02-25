697e9ea91006b57ece78b2e286ae220bbdf3ba34
Matthew Macdonald-Wallace <matt@doics.co>
2026-02-25 17:43:34 +0000

    Instrument PostgreSQL DB calls with OpenTelemetry

--- basic_otel/basic_otel/settings.py
+++ basic_otel/basic_otel/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'spacedir'
+    'spacedir.apps.SpacedirConfig'
 ]
 
 MIDDLEWARE = [
--- basic_otel/spacedir/apps.py
+++ basic_otel/spacedir/apps.py
@@ -1,6 +1,27 @@
 from django.apps import AppConfig
+from django.conf import settings
+
+_DB_INSTRUMENTED = False
 
 
 class SpacedirConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'spacedir'
+
+    def ready(self):
+        global _DB_INSTRUMENTED
+        if _DB_INSTRUMENTED:
+            return
+
+        db_engine = settings.DATABASES.get("default", {}).get("ENGINE", "")
+        if "postgresql" not in db_engine:
+            return
+
+        try:
+            from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
+
+            Psycopg2Instrumentor().instrument(enable_commenter=True)
+            _DB_INSTRUMENTED = True
+        except Exception:
+            # Keep startup resilient if OTEL instrumentation isn't available.
+            return
